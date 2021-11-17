ad324db37a57dffdd29b632648c8a20ccb2d97fc
Joel Riesen <57596679+jriesen-MSDS@users.noreply.github.com>
2021-11-17 06:22:43 -0600

    Update app.py

--- app.py
+++ app.py
@@ -20,16 +20,17 @@ def hello(name):
     return jsonify(greeting)
 
 @app.route('/Info/<response>')
-url = "https://6l6l7n7vtb.execute-api.us-east-2.amazonaws.com/test/predict"
+def resp(response):
+    url = "https://6l6l7n7vtb.execute-api.us-east-2.amazonaws.com/test/predict"
 
-payload="{\"data\":\"0,-0.34147611300851444,0.18202662446267728,310000.0,2.0,2.0,1.0,44.0,-1.0,-1.0,-2.0,-2.0,-2.0,-2.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0\"}"
-headers = {
-  'Content-Type': 'application/json'
-}
+    payload="{\"data\":\"0,-0.34147611300851444,0.18202662446267728,310000.0,2.0,2.0,1.0,44.0,-1.0,-1.0,-2.0,-2.0,-2.0,-2.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0\"}"
+    headers = {
+    'Content-Type': 'application/json'
+    }
 
-response = requests.request("POST", url, headers=headers, data=payload)
+    response = requests.request("POST", url, headers=headers, data=payload)
 
-print(response.text)
+    print(response.text)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
